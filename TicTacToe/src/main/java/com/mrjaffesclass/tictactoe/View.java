d95a337f4a5abb33396ae4920d7fb793af79cbae
lancecarinugan <lance.carinugan@mrjaffesclass.com>
2023-03-06 01:20:16 -0800

    Sunday 03/05/23, ALMOST FINISHED

--- TicTacToe/src/main/java/com/mrjaffesclass/tictactoe/View.java
+++ TicTacToe/src/main/java/com/mrjaffesclass/tictactoe/View.java
@@ -220,9 +220,46 @@ public class View extends javax.swing.JFrame {
         JButton button = (JButton)evt.getSource();
         String player = (this.whoseTurn) ? "X" : "O";
         button.setText(player);
-        this.whoseTurn = !this.whoseTurn;
+        if (!winner.equals("")) {
+            jLabel1.setText(winner + "WINS THE GAME!");
+            this.gameOver = true;
+            }
+        this.whoseTurn = !this.whoseTurn
+        }
     }//GEN-LAST:event_onClick
+    private String isWinner() {
+  // Get the text contents of each button.  
+  // Be sure you're accessing the
+  // buttons in the order you want.  
+  // The Netbeans UI sometimes mixes up
+  // the numbers on the button names so they're not in the 
+  // order you expect
+  String[][] status = new String[3][3];
+  status[0][0] = jButton1.getText();
+  status[0][1] = jButton2.getText();
+  status[0][2] = jButton3.getText();
+  status[1][0] = jButton4.getText();
+  status[1][1] = jButton5.getText();
+  status[1][2] = jButton6.getText();
+  status[2][0] = jButton7.getText();
+  status[2][1] = jButton8.getText();
+  status[2][2] = jButton9.getText();
+  // Check the rows and columns for a tic tac toe
+  for (int i=0; i<3; i++) {
+    if (status[i][0].equals(status[i][1]) && status[i][0].equals(status[i][2]))
+      return status[i][0];
+    if (status[0][i].equals(status[1][i]) && status[0][i].equals(status[2][i]))
+      return status[0][i];
+  }
+  // Check the diagonals
+  if (status[0][0].equals(status[1][1]) && status[0][0].equals(status[2][2]))
+    return status[0][0];
+  if (status[0][2].equals(status[1][1]) && status[0][2].equals(status[2][0]))
+    return status[0][2];
 
+  // If we haven't found it, then return a blank string
+  return "";
+}
     /**
      * @param args the command line arguments
      */
